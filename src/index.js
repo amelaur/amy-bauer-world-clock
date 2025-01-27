cb355031b126f99f4a1a855fe82d194f8dfa0892
amelaur <amybauerdance@gmail.com>
2025-01-27 13:16:30 -0700

    added a space for AM/PM after selection

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function updateCity(event) {
       <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div> 
       <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
-    "A"
+    " A"
   )}</small></div>
     </div>`;
 }
